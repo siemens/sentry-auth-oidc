189444ff029c16d989807460a517ac343dac91b5
Max Wittig <max.wittig95@gmail.com>
2019-05-24 10:52:01 +0200

    Update .travis.yml

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 # Run tests against sqlite for simplicity
-os.environ.setdefault('DB', 'sqlite')
+os.environ.setdefault('DB', 'postgres')
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__)))
 
